e1b9c0db243b809e213d6ddd9e8250642bb1b181
Ludovic Rivallain <ludovic.rivallain@gmail.com>
2018-07-26 12:27:21 +0200

    fix sitemap

--- run-jekyll-local-as-github.sh
+++ run-jekyll-local-as-github.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export LANG=en_US.UTF-8
 python3 tag_generator.py
-jekyll serve --config _config.yml -H 0.0.0.0 --incremental
+bundle exec jekyll serve --config _config.yml -H 0.0.0.0 $1
